71fa0ffd14ad41770f816bd2939afd4774772e5e
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-04-03 07:08:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login/login.jsx";
-import Register from "./components/register/register.jsx";
-import { RequireToken } from './components/Auth/Auth.jsx'
-import Dashboard from "./components/dashboard/dashboard.jsx";
-import Home from "./components/home.jsx";
-import Employee from "./components/employee.jsx";
-import Profile from "./components/profile.jsx";
-import AddEmployee from "./components/addEmployee/addEmployee.jsx";
-import EditEmployee from './components/editEmployee/editEmployee.jsx'
-
-
 function App() {
   return (
     <div className="app">
@@ -21,19 +9,17 @@ function App() {
 
           <Route path='/' element={
             <RequireToken>
-              <Dashboard />
+              <Routes>
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/employee' element={<Employee />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/create' element={<AddEmployee />} />
+                <Route path='/employeeedit/:id' element={<EditEmployee />} />
+              </Routes>
             </RequireToken>
-          }>
-           
-            <Route path='/employee' element={<Employee />}></Route>
-            <Route path='/profile' element={<Profile />}></Route>
-            <Route path='/create' element={<AddEmployee />}></Route>
-            <Route path='/employeeedit/:id' element={<EditEmployee />}></Route>
-          </Route>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
-export default App;
