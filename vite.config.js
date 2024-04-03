cc52a2b86066a0c7be985f82e31690a2a9703043
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-04-03 06:10:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: 'https://adminapi-fcjc.onrender.com',
+  base: '/',
   plugins: [react()],
 });
+
